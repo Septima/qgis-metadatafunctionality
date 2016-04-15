b0bb6e0687979ad46398b595f675355c4e2a01ee
Bernhard Snizek <bs@metascapes.org>
2016-04-15 13:40:06 +0200

    commenced work on i18n

---
+++ src/MetadataFunctionality/i18n/metaman.js
@@ -0,0 +1,124 @@
+ ? xml : version = "1.0";
+encoding = "utf-8" ?  >
+    !DOCTYPE : TS >
+    version;
+"2.0";
+language = "en_US" >
+    MetadataDockWidgetBase < /name>
+    < message >
+    MetaMan;
+History < /source>
+    < translation;
+type = "unfinished" > /translation>
+    < /message>
+    < /context>
+    < context >
+    MetadataFunctionalityDialogBase < /name>
+    < message >
+    MetadataFunctionality < /source>
+    < translation;
+type = "unfinished" > /translation>
+    < /message>
+    < message >
+    Navn;
+/source>
+    < translation;
+type = "unfinished" > /translation>
+    < /message>
+    < message >
+    Beskrivelse;
+/source>
+    < translation;
+type = "unfinished" > /translation>
+    < /message>
+    < message >
+    Journal;
+nr.;
+/source>
+    < translation;
+type = "unfinished" > /translation>
+    < /message>
+    < message >
+    Dato(oprettelse);
+/source>
+    < translation;
+type = "unfinished" > /translation>
+    < /message>
+    < message >
+    Ansvarlig;
+center / medarbejder;
+/source>
+    < translation;
+type = "unfinished" > /translation>
+    < /message>
+    < message;
+utf8 = "true" >
+    Hvilke;
+projekter / Wor;
+filen;
+indgår: /source>
+    < translation;
+type = "unfinished" > /translation>
+    < /message>
+    < message >
+;
+/source>
+    < translation;
+type = "unfinished" > /translation>
+    < /message>
+    < message >
+    Gem < /source>
+    < translation;
+type = "unfinished" > Save < /translation>
+    < /message>
+    < message;
+utf8 = "true" >
+    Tilføj < /source>
+    < translation;
+type = "unfinished" > Add < /translation>
+    < /message>
+    < message >
+    Slet < /source>
+    < translation;
+type = "unfinished" > Delete < /translation>
+    < /message>
+    < message >
+    Create;
+Metadata;
+Table < /source>
+    < translation;
+type = "unfinished" > /translation>
+    < /message>
+    < message >
+    Database;
+/source>
+    < translation;
+type = "unfinished" > /translation>
+    < /message>
+    < message >
+    Test;
+Connection < /source>
+    < translation;
+type = "unfinished" > /translation>
+    < /message>
+    < message >
+    Database;
+user: /source>
+    < translation;
+type = "unfinished" > /translation>
+    < /message>
+    < message >
+    Database;
+password: /source>
+    < translation;
+type = "unfinished" > /translation>
+    < /message>
+    < message >
+    Database;
+Definition < /source>
+    < translation;
+type = "unfinished" > /translation>
+    < /message>
+    < /context>
+    < /TS>;
+//# sourceMappingURL=metaman.js.map
