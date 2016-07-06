d12cca02a453ab9bee714394a92f4634099519e8
mie winstrup <mie@septima.dk>
2016-07-06 10:34:10 +0200

    Oversættelse fra engelsk til dansk

---
+++ src/MetadataFunctionality/i18n/da.ts
@@ -0,0 +1,349 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0">
+<context>
+    <name></name>
+    <message>
+        <location filename="metadata_db_linker.py" line="154"/>
+        <source>Rename</source>
+        <translation>Omdøb</translation>
+    </message>
+    <message>
+        <location filename="metadata_db_linker.py" line="155"/>
+        <source>Delete</source>
+        <translation>Slet</translation>
+    </message>
+    <message>
+        <location filename="metadata_db_linker.py" line="159"/>
+        <source>Add to canvas</source>
+        <translation>Føj til kanvas</translation>
+    </message>
+    <message>
+        <location filename="metadata_db_linker.py" line="163"/>
+        <source>Re-connect</source>
+        <translation>Re-connect</translation>
+    </message>
+    <message>
+        <location filename="metadata_db_linker.py" line="164"/>
+        <source>Remove</source>
+        <translation>Fjern</translation>
+    </message>
+    <message>
+        <location filename="metadata_db_linker.py" line="167"/>
+        <source>New Connection...</source>
+        <translation>Ny Forbindelse...</translation>
+    </message>
+    <message>
+        <location filename="metadata_db_linker.py" line="171"/>
+        <source>Metadata...</source>
+        <translation>Metadata...</translation>
+    </message>
+</context>
+<context>
+    <name>@default</name>
+    <message>
+        <location filename="test_translations.py" line="48"/>
+        <source>Good morning</source>
+        <translation>Godmorgen</translation>
+    </message>
+</context>
+<context>
+    <name>MetadataDbLinker</name>
+    <message>
+        <location filename="metadata_db_linker.py" line="280"/>
+        <source>&amp;{}</source>
+        <translation>&amp;{}</translation>
+    </message>
+    <message>
+        <location filename="metadata_db_linker.py" line="256"/>
+        <source>Enter or edit metadata</source>
+        <translation>Indtast eller redigér metadata</translation>
+    </message>
+    <message>
+        <location filename="metadata_db_linker.py" line="267"/>
+        <source>Settings</source>
+        <translation>Indstillinger</translation>
+    </message>
+</context>
+<context>
+    <name>MetadataDialog</name>
+    <message>
+        <location filename="dialog_metadata.py" line="168"/>
+        <source>No taxon url and/or taxonomy</source>
+        <translation>Ingen taxon-url og/eller taxonomi</translation>
+    </message>
+    <message>
+        <location filename="dialog_metadata.py" line="561"/>
+        <source>Please!</source>
+        <translation>Vær venlig!</translation>
+    </message>
+    <message>
+        <location filename="dialog_metadata.py" line="219"/>
+        <source>Either database is unavailable, table with metadata does not exist or wrong access rights.</source>
+        <translation>Database er enten utilgængelig, metadatatabellen eksisterer ikke eller der er forkert indtastede adgangsrettigheder til databasen </translation>
+    </message>
+    <message>
+        <location filename="dialog_metadata.py" line="435"/>
+        <source>Error selecting data.</source>
+        <translation>Fejl opstod ved udvælgelse af data</translation>
+    </message>
+    <message>
+        <location filename="dialog_metadata.py" line="552"/>
+        <source>See log for error details.</source>
+        <translation>Se log for detaljer om fejlen</translation>
+    </message>
+    <message>
+        <location filename="dialog_metadata.py" line="326"/>
+        <source>Error deleting data.</source>
+        <translation>Der opstod en fejl ved sletning af data</translation>
+    </message>
+    <message>
+        <location filename="dialog_metadata.py" line="511"/>
+        <source>Error updating data.</source>
+        <translation>Der opstod en fejl ved opdatering af data</translation>
+    </message>
+    <message>
+        <location filename="dialog_metadata.py" line="552"/>
+        <source>Error inserting data.</source>
+        <translation>Der opstod en fejl ved indsættelse af data</translation>
+    </message>
+    <message>
+        <location filename="dialog_metadata.py" line="561"/>
+        <source>Remember to select a table.</source>
+        <translation>Husk at udvælge en tabel</translation>
+    </message>
+    <message>
+        <location filename="dialog_metadata.py" line="168"/>
+        <source>Please enter an url and taxonomy for Taxon service in settings.</source>
+        <translation>Indtast en url og taxonomi til Taxon service i indstillingsdialogen</translation>
+    </message>
+</context>
+<context>
+    <name>MetadataDockWidgetBase</name>
+    <message>
+        <location filename="dockwidget.ui" line="14"/>
+        <source>MetaMan History</source>
+        <translation>Metadata historik</translation>
+    </message>
+</context>
+<context>
+    <name>MetadataFunctionalityDialogBase</name>
+    <message>
+        <location filename="dialog_metadata.ui" line="29"/>
+        <source>Metadata-DB-linker</source>
+        <translation>Metadata-DB-linker</translation>
+    </message>
+    <message>
+        <location filename="dialog_metadata.ui" line="42"/>
+        <source>Metadata information</source>
+        <translation>Metadatainformation</translation>
+    </message>
+    <message>
+        <location filename="dialog_metadata.ui" line="54"/>
+        <source>The file to which you want to add or modify metadata:</source>
+        <translation>Filen hvor metadata skal tilføjes eller redigeres</translation>
+    </message>
+    <message>
+        <location filename="dialog_metadata.ui" line="79"/>
+        <source>Description of content:</source>
+        <translation>Beskrivelse af indhold:</translation>
+    </message>
+    <message>
+        <location filename="dialog_metadata.ui" line="89"/>
+        <source>Descriptive name:</source>
+        <translation>Beskrivende navn:</translation>
+    </message>
+    <message>
+        <location filename="dialog_metadata.ui" line="124"/>
+        <source>Metadatainformation</source>
+        <translation>Metadatainformation</translation>
+    </message>
+    <message>
+        <location filename="dialog_metadata.ui" line="131"/>
+        <source>Projects in which the file is used:</source>
+        <translation>Projekter hvori filen indgår:</translation>
+    </message>
+    <message>
+        <location filename="dialog_metadata.ui" line="138"/>
+        <source>Kle-journal number:</source>
+        <translation>Kle-journalnummer:</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="dialog_metadata.ui" line="145"/>
+        <source>Responsible centre or employée</source>
+        <translation>Ansvarligt center eller medarbejder</translation>
+    </message>
+    <message>
+        <location filename="dialog_metadata.ui" line="202"/>
+        <source>Save metadata</source>
+        <translation>Gem metadata</translation>
+    </message>
+    <message>
+        <location filename="dialog_metadata.ui" line="220"/>
+        <source>Delete metadata</source>
+        <translation>Slet metadata</translation>
+    </message>
+    <message>
+        <location filename="dialog_metadata.ui" line="246"/>
+        <source>Search Kle</source>
+        <translation>Opmærk med Kle</translation>
+    </message>
+    <message>
+        <location filename="dialog_metadata.ui" line="268"/>
+        <source>Suggested KLE-numbers</source>
+        <translation>KLe-forslag</translation>
+    </message>
+    <message>
+        <location filename="dialog_metadata.ui" line="284"/>
+        <source>Latest changes:</source>
+        <translation>Sidste ændring:</translation>
+    </message>
+    <message>
+        <location filename="dialog_metadata.ui" line="294"/>
+        <source>dd/MM-yyyy HH:mm</source>
+        <translation>dd/MM-yyyy HH:mm</translation>
+    </message>
+    <message>
+        <location filename="dialog_metadata.ui" line="308"/>
+        <source>Help</source>
+        <translation>Hjælp</translation>
+    </message>
+    <message>
+        <location filename="dialog_settings.ui" line="20"/>
+        <source>Settings</source>
+        <translation>Indstillinger</translation>
+    </message>
+    <message>
+        <location filename="dialog_settings.ui" line="32"/>
+        <source>Taxon taxonomy:</source>
+        <translation>Taxon taxonomi:</translation>
+    </message>
+    <message>
+        <location filename="dialog_settings.ui" line="56"/>
+        <source>Schema:</source>
+        <translation>Schema:</translation>
+    </message>
+    <message>
+        <location filename="dialog_settings.ui" line="82"/>
+        <source>Taxon url:</source>
+        <translation>Taxon url:</translation>
+    </message>
+    <message>
+        <location filename="dialog_settings.ui" line="111"/>
+        <source>Database name:</source>
+        <translation>Databasenavn:</translation>
+    </message>
+    <message>
+        <location filename="dialog_settings.ui" line="121"/>
+        <source>Database password:</source>
+        <translation>Database password:</translation>
+    </message>
+    <message>
+        <location filename="dialog_settings.ui" line="131"/>
+        <source>Database user:</source>
+        <translation>Databasebruger:</translation>
+    </message>
+    <message>
+        <location filename="dialog_settings.ui" line="166"/>
+        <source>Information about the database where the table with metadata is stored:</source>
+        <translation>Information om databasen hvori tabellen med metadata er gemt:</translation>
+    </message>
+    <message>
+        <location filename="dialog_settings.ui" line="176"/>
+        <source>Metadata table:</source>
+        <translation>Metadata-tabel:</translation>
+    </message>
+    <message>
+        <location filename="dialog_settings.ui" line="243"/>
+        <source>Save settings or cancel</source>
+        <translation>Gem indstillinger eller afbryd</translation>
+    </message>
+    <message>
+        <location filename="dialog_settings.ui" line="262"/>
+        <source>Additional settings:</source>
+        <translation>Yderligere indstillinger:</translation>
+    </message>
+    <message>
+        <location filename="dialog_settings.ui" line="275"/>
+        <source>Only required the first time you use the plugin</source>
+        <translation>Er kun nødvendigt den første gang du bruger plugin&apos;et</translation>
+    </message>
+    <message>
+        <location filename="dialog_settings.ui" line="288"/>
+        <source>Test connection to the metadata-table</source>
+        <translation>Test forbindelse til metadata-tabel</translation>
+    </message>
+    <message>
+        <location filename="dialog_settings.ui" line="291"/>
+        <source>Test connection to database</source>
+        <translation>Test forbindelse til databasen</translation>
+    </message>
+    <message>
+        <location filename="dialog_settings.ui" line="301"/>
+        <source>Host:</source>
+        <translation>Host:</translation>
+    </message>
+    <message>
+        <location filename="dialog_settings.ui" line="330"/>
+        <source>Port:</source>
+        <translation>Port:</translation>
+    </message>
+    <message>
+        <location filename="dialog_settings.ui" line="342"/>
+        <source>KLE:</source>
+        <translation>KLE:</translation>
+    </message>
+    <message>
+        <location filename="dialog_settings_db_def.ui" line="14"/>
+        <source>MetadataFunctionality</source>
+        <translation>Metadatafunktionalitet</translation>
+    </message>
+    <message>
+        <location filename="dialog_settings.ui" line="278"/>
+        <source>Metadata table (sql)</source>
+        <translation>Metadata table (sql)</translation>
+    </message>
+    <message>
+        <location filename="dialog_metadata.ui" line="317"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;MS Shell Dlg 2&apos;; font-size:8.25pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:8pt;&quot;&gt;With Metadata-DB-linker you can use QGIS to enter metadata to new and existing tables stored in a Postgresdatabase.&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:8pt;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:8pt; font-weight:600;&quot;&gt;The first time you use the plugin:&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:8pt;&quot;&gt;1. Create the table in which metadata will be stored. See sql-definition of the table under the Settings-dialog. &lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:8pt;&quot;&gt;2. In the settings-dialog: Specify host, port, schema, tablename, username and password to the database in which the metadata-table is stored. If you want to automatically assign kle-journalnumbers, specify taxonurl and taxon taxonomy.&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:8pt;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:8pt; font-weight:600;&quot;&gt;How to use the plugin:&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:8pt; text-decoration: underline;&quot;&gt;Adding metadata to a new table with DB Manager:&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:8pt;&quot;&gt;The dialogbox to enter metadata is automatically launched when a new table is added to a postgresdatabase with the import function in DB Manager or created as a new table with DB Manager. Enter metadata in the fields and save.&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:8pt;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:8pt; text-decoration: underline;&quot;&gt;Adding or changing metadata to an existing table:&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:8pt;&quot;&gt;Metadata to existing tables can be added or changed with the dialog from either the Plugin-menu or the plugin-button. In the dialog, navigate to the table to which you want to add or change metadata. Enter metadata in the fields and save.&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="dialog_settings.py" line="128"/>
+        <source>Information</source>
+        <translation>Information</translation>
+    </message>
+    <message>
+        <location filename="dialog_settings.py" line="128"/>
+        <source>DB structure and connection OK.</source>
+        <translation>DB-struktur og forbindelse OK</translation>
+    </message>
+    <message>
+        <location filename="dialog_settings.py" line="134"/>
+        <source>Warning</source>
+        <translation>Advarsel</translation>
+    </message>
+    <message>
+        <location filename="dialog_settings.py" line="134"/>
+        <source>Either structure of database or connection is broken</source>
+        <translation>Der er fejl med databasestruktur eller forbindelse</translation>
+    </message>
+</context>
+</TS>
