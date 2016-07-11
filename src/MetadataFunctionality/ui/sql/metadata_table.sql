040b44d572318e07238ec948ea176f5fb6cb8733
klavs <klavs@septima.dk>
2016-07-11 10:35:34 +0200

    Search function

--- src/MetadataFunctionality/ui/sql/metadata_table.sql
+++ src/MetadataFunctionality/ui/sql/metadata_table.sql
@@ -1,4 +1,7 @@
-CREATE TABLE schema.metadata (
+-- These statements create a table and a function
+-- Please replace ALL occurrences [schema].[metadata]
+
+CREATE TABLE [schema].[metadata] (
     name varchar,
     description varchar,
     kle_no varchar,
@@ -14,4 +17,47 @@ CREATE TABLE schema.metadata (
     CONSTRAINT pk_metadata PRIMARY KEY (guid)
 )
 WITH (OIDS=FALSE);
-ALTER TABLE schema.metadata OWNER TO owner;
+ALTER TABLE [schema].[metadata] OWNER TO owner;
+
+create or replace function [schema]._getMetaDataMatches(varchar, int)
+returns table (name varchar, description varchar, host varchar, db varchar, port integer, schema varchar, sourcetable varchar)
+    as $$
+    WITH QUERY AS (
+                SELECT $1::text AS VALUE
+            ),
+            st AS (
+                SELECT REGEXP_SPLIT_TO_TABLE(value, E'\\s+') search_token
+                FROM QUERY
+            )
+            SELECT
+              name,
+              description,
+              host,
+              db,
+              port,
+              schema,
+              sourcetable
+            FROM [schema].[metadata] m1
+            WHERE NOT EXISTS
+            (
+                SELECT *
+                FROM st
+                WHERE NOT EXISTS
+                (
+                    SELECT 1
+                    FROM [schema].[metadata] m2
+                    WHERE
+                    (
+                        m2.responsible ILIKE search_token
+                        OR m2.description ILIKE '%' || search_token || '%'
+                        OR m2.name ILIKE search_token || '%'
+                        OR m2.db ILIKE search_token
+                        OR m2.sourcetable ILIKE search_token || '%'
+                        OR m2.project ILIKE '%' || search_token || '%'
+                    )
+                AND m1.guid = m2.guid
+                )
+            )
+            LIMIT $2;
+    $$
+    language sql;
